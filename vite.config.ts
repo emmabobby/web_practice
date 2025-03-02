242d57ace1d2005c2268c9dc257ccfc9828ed5d6
emmabobby <evansjota@gmail.com>
2025-03-02 23:12:36 +0100

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0', // Bind to all network interfaces
+    allowedHosts: [
+      'webmailer-6q0a.onrender.com', // Add your Render host here
+    ],
   },
   plugins: [
     tailwindcss(),
   ],
-})
+});
